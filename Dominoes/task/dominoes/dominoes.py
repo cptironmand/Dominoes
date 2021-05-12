98d4e290ebf2bdcd5dd1bfec3873442a6d587461
cptironmand <jaymandjunk@gmail.com>
2021-05-11 19:52:52 -0500

    Version 1.0

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -1,40 +1,148 @@
-'''
+"""
 This program will create a set of dominoes, split them up between stock, computer, player,
 ID the snake, determine who goes first, and output the variables.
-'''
+"""
+
 
 # generate random integer values
 from random import randint
 
-# generate a random integer
-secret_value = randint(1, 28)
+# build global variables
+starter_piece = []
 
 
-class Domino:
-    # list of all dominoes
-    all_dominoes = []
+def build_dominoes():
+    """
+    Create the dominoes and their defaults, and put them into a dictionary
+    """
+    # Create the dominoes.
+    stack = []
+    for i in range(0, 7):
+        for j in range(0, 7):
+            var = [i, j]
+            if ([i, j] in stack) or ([j, i] in stack):
+                pass
+            else:
+                stack.append(var)
 
-    def __init__(self, index_num, value, assigned='stock', played=False):
-        self.index_num = index_num
-        self.value = value
-        self.assigned = assigned
-        self.played = played
-        # add current river to the list of all rivers
-        Domino.all_dominoes.append(self)
+    # Build the default domino values.  They are 'assigned' to 'none', have not been played, and default as not a snake.
+    working_dict = {}
 
-# Create the dominoes.  Assign each an index, whether it's been played, who owns it, and the piece value
-dominoes = []
-stack = []
-ind = 0
-for i in range(0, 7):
-    for j in range(0, 7):
-        var = [i, j]
-        if ([i, j] in stack) or ([j, i] in stack):
-            pass
+    key = {'assigned'}
+    value = 'none'
+    new_dict = dict.fromkeys(key, value)
+    working_dict.update(new_dict)
+
+    key = {'played'}
+    value = False
+    new_dict = dict.fromkeys(key, value)
+    working_dict.update(new_dict)
+
+    key = {'snake'}
+    value = False
+    new_dict = dict.fromkeys(key, value)
+    working_dict.update(new_dict)
+
+    # Create the dictionary of dominoes
+    dominoes = {}
+    ind = 0
+    for i in stack:
+        key = {'value'}
+        value = i
+        inside_dict = dict.fromkeys(key, value)
+        inside_dict.update(working_dict)
+
+        key = {ind}
+        value = inside_dict
+        outside_dict = dict.fromkeys(key, value)
+        dominoes.update(outside_dict)
+        ind += 1
+
+    # ID the possible snakes and assign them their value equal to one of the digits
+    for i in dominoes:
+        if dominoes[i]['value'][0] == dominoes[i]['value'][1]:
+            dominoes[i]['snake'] = dominoes[i]['value'][0]
+    return dominoes
+
+
+def shuffle_dominoes(dominoes):
+    """
+    Randomly assign the dominoes to the computer, the player, and leave the rest in the stock
+    """
+    picked = []
+    num = 1
+    while len(picked) < 14:
+        secret_value = randint(0, 27)
+        if secret_value not in picked:
+            picked.append(secret_value)
+            if num % 2 == 0:
+                dominoes[secret_value]['assigned'] = 'computer'
+            else:
+                dominoes[secret_value]['assigned'] = 'player'
+            num += 1
         else:
-            stack.append(var)
-            dominoes.append([ind, False, 'stock', var])
-            ind +=1
+            continue
+    return dominoes
 
 
+def assign_stacks(dominoes):
+    """
+    Take the dominoes as a variable and return the current player, computer, and stock stacks
+    """
 
+    count = 0
+    stock = []
+    computer = []
+    player = []
+    for i in dominoes:
+        if dominoes[i]['assigned'] == 'none':
+            stock.append(dominoes[i]['value'])
+        elif dominoes[i]['assigned'] == 'computer':
+            computer.append(dominoes[i]['value'])
+        elif dominoes[i]['assigned'] == 'player':
+            player.append(dominoes[i]['value'])
+        count += 1
+
+    return player, computer, stock
+
+
+def first_play(dominoes, snake):
+    """
+    Find the starter domino (snake), determine which player has it, play the piece, update game pieces
+    and return them
+    """
+
+    for i in dominoes:
+        if dominoes[i]['assigned'] == 'computer':
+            if dominoes[i]['snake']:
+                snake = ['computer', i]
+        elif dominoes[i]['assigned'] == 'player':
+            if dominoes[i]['snake']:
+                snake = ['player', i]
+    dominoes[snake[1]]['assigned'] = 'played'
+
+    if snake[0] == 'player':
+        next_turn = 'computer'
+    else:
+        next_turn = 'player'
+    snake = [dominoes[snake[1]]['value']]
+
+    return next_turn, dominoes, snake
+
+
+# --- MAIN BODY OF GAME --- #
+game_pieces = build_dominoes()
+
+# Create player_pieces, cpu_pieces, remaining_stack, and the starter_piece (can't be empty)
+while not starter_piece:
+    game_pieces = shuffle_dominoes(game_pieces)
+    game_turn, game_pieces, starter_piece = first_play(game_pieces, starter_piece)
+
+player_pieces, cpu_pieces, remaining_stack = assign_stacks(game_pieces)
+
+# Print required outputs for 1.0
+print(f'Stock pieces: {remaining_stack}')
+print(f'Computer pieces: {cpu_pieces}')
+print(f'Player pieces: {player_pieces}')
+print(f'Domino snake: {starter_piece}')
+print(f'Status: {game_turn}')
